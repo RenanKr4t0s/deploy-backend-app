5fc4a698f6ee67d671f8391ff88329f17f075c64
RenanKr4t0s <redrums88@gmail.com>
2024-07-04 17:31:57 -0300

    Mudancas de config pra rodar no server

--- config/db.js
+++ config/db.js
@@ -5,7 +5,8 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const connection = mysql.createConnection({
-  host: 'localhost',
+  port: process.env.DB_PORT,
+  host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
--- env sample.sh
+++ env sample.sh
@@ -1,4 +1,5 @@
-export PORT='3001'
+export DB_HOST ='localhost'
+export DB_PORT='3001'
 export DB_USER="jhonDooe"
 export DB_PASSWORD="ImN3O"
 export DB_NAME="data_deploy"
